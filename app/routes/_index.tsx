8963f3890bfe78c68051a8181d2277edf43eaa9e
scossar <scossar3@gmail.com>
2024-05-06 21:43:23 -0700

    update to latest version of ZalgEditor

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,6 +1,8 @@
-import type { MetaFunction } from "@remix-run/node";
+import type { ActionFunctionArgs, MetaFunction } from "@remix-run/node";
+import { useFetcher } from "@remix-run/react";
 import { ClientOnly } from "~/components/ClientOnly";
 import Composer from "~/components/zalgEditor/Composer";
+import { CustomFetcher } from "~/components/zalgEditor/plugins/SubmitPlugin";
 
 export const meta: MetaFunction = () => {
   return [
@@ -12,7 +14,17 @@ export const meta: MetaFunction = () => {
   ];
 };
 
+export async function action({ request }: ActionFunctionArgs) {
+  const body = await request.formData();
+  const markdown = body?.get("markdown");
+  const html = body?.get("html");
+
+  console.log(`markdown: ${markdown}, html: ${html}`);
+  return null;
+}
+
 export default function Index() {
+  const fetcher = useFetcher();
   return (
     <div className="mx-auto max-w-screen-sm">
       <h1 className="text-3xl">Lexical playground</h1>
@@ -21,7 +33,9 @@ export default function Index() {
           <div className="max-w-full p-2 border rounded-sm min-h-48 border-slate-500"></div>
         }
       >
-        {() => <Composer />}
+        {() => (
+          <Composer submitType="html" fetcher={fetcher as CustomFetcher} />
+        )}
       </ClientOnly>
     </div>
   );
